f68ef0490392f606142ff97567ddca80b074f24a
Sayed Arif Shah <arifsayed9325@gmail.com>
2024-11-23 21:16:06 -0500

    Error Handling, Added Status COde Pages

--- HPlusSport.Security.Web/Program.cs
+++ HPlusSport.Security.Web/Program.cs
@@ -45,6 +45,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseExceptionHandler("/Home/Error");
+
+app.UseStatusCodePages();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
